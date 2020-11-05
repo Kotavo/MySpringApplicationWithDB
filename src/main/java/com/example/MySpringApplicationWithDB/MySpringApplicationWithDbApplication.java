ffbd7a839c97636a30e858b85372fdec00233c85
Dmitry Sergeev <Borland32@yandex.ru>
2020-11-05 15:09:22 +0300

    Added @EnableTransactionManagement

--- src/main/java/com/example/MySpringApplicationWithDB/MySpringApplicationWithDbApplication.java
+++ src/main/java/com/example/MySpringApplicationWithDB/MySpringApplicationWithDbApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import java.security.Security;
 
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class MySpringApplicationWithDbApplication {
 
     public static void main(String[] args) {
