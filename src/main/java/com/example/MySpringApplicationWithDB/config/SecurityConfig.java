7e1a81d248b4394dd4e9bd02c664cf0157021288
Dmitry Sergeev <Borland32@yandex.ru>
2020-11-30 13:44:45 +0300

    fix

--- src/main/java/com/example/MySpringApplicationWithDB/config/SecurityConfig.java
+++ src/main/java/com/example/MySpringApplicationWithDB/config/SecurityConfig.java
@@ -3,17 +3,13 @@ package com.example.MySpringApplicationWithDB.config;
 import com.example.MySpringApplicationWithDB.security.JwtConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
